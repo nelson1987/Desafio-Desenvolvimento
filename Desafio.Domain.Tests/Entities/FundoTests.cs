93fe41f9b1be25ff7503d31136cce08da98f2170
Nelson <nelson.ash@outlook.com>
2019-09-04 18:38:14 -0300

    Criacao de modulo de Testes de Dominio

---
+++ Desafio.Domain.Tests/Entities/FundoTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Desafio.Domain.Tests.Entities
+{
+    public class FundoTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CriarFundo()
+        {
+            Assert.Pass();
+        }
+    }
+}
